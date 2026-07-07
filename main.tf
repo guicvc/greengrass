b9e7130d2142e9bee2ca2804f2effbe9a586b42a
Guilherme CV Costa <guicvc@users.noreply.github.com>
2026-07-06 22:13:41 -0300

    Second run

--- main.tf
+++ main.tf
@@ -50,6 +50,24 @@ resource "aws_iot_policy_attachment" "greengrass_core" {
 }
 
 resource "aws_iot_thing_principal_attachment" "greengrass_core" {
-  thing      = aws_iot_thing.greengrass_core.name
-  principal  = aws_iot_certificate.greengrass_core.arn
+  thing     = aws_iot_thing.greengrass_core.name
+  principal = aws_iot_certificate.greengrass_core.arn
+}
+
+resource "local_file" "greengrass_certificate" {
+  content         = aws_iot_certificate.greengrass_core.certificate_pem
+  filename        = "${path.module}/generated/${var.name_prefix}-certificate.pem.crt"
+  file_permission = "0600"
+}
+
+resource "local_file" "greengrass_private_key" {
+  content         = aws_iot_certificate.greengrass_core.private_key
+  filename        = "${path.module}/generated/${var.name_prefix}-private.pem.key"
+  file_permission = "0600"
+}
+
+resource "local_file" "greengrass_public_key" {
+  content         = aws_iot_certificate.greengrass_core.public_key
+  filename        = "${path.module}/generated/${var.name_prefix}-public.pem.key"
+  file_permission = "0600"
 }
--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,27 @@ output "greengrass_iot_certificate_arn" {
   value       = aws_iot_certificate.greengrass_core.arn
 }
 
+output "greengrass_certificate_pem" {
+  description = "The AWS IoT certificate PEM for the Greengrass core."
+  value       = aws_iot_certificate.greengrass_core.certificate_pem
+  sensitive   = true
+}
+
+output "greengrass_private_key" {
+  description = "The private key for the Greengrass core certificate."
+  value       = aws_iot_certificate.greengrass_core.private_key
+  sensitive   = true
+}
+
+output "greengrass_certificate_files" {
+  description = "Paths to the generated certificate, private key, and public key files."
+  value = {
+    certificate = local_file.greengrass_certificate.filename
+    private_key = local_file.greengrass_private_key.filename
+    public_key  = local_file.greengrass_public_key.filename
+  }
+}
+
 output "greengrass_iot_policy_name" {
   description = "Name of the IoT policy attached to the Greengrass core."
   value       = aws_iot_policy.greengrass_core.name
